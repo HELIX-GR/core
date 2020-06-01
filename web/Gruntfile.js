2e044aa2d181c83450f2a65b06f6a2729c1d2b7c
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-06-01 14:42:12 +0300

    [frontend][web] Remove JavaScript documentation tasks

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -158,20 +158,6 @@ module.exports = function (grunt) {
       options: {
         mode: '0644',
       },
-      'apidoc': {
-        expand: true,
-        filter: 'isFile',
-        cwd: 'apidoc/docs/',
-        src: ['**/*'],
-        dest: '<%= targetDir %>/docs/',
-      },
-      'jsdoc': {
-        expand: true,
-        filter: 'isFile',
-        cwd: 'jsdoc',
-        src: ['**/*'],
-        dest: '<%= targetDir %>/docs/core-client',
-      },
       'helix-core-scripts': {
         files: [{
           expand: true,
@@ -266,41 +252,6 @@ module.exports = function (grunt) {
       },
     },
 
-    // Generate API documentation
-    apidoc: {
-      'core-action': {
-        src: "apidoc/src/core-action",
-        dest: "apidoc/docs/core-action",
-        template: "apidoc/template",
-        options: {
-          debug: false,
-          includeFilters: [".*\\.js$"]
-        }
-      },
-      'core-api': {
-        src: "apidoc/src/core-api",
-        dest: "apidoc/docs/core-api",
-        template: "apidoc/template",
-        options: {
-          debug: false,
-          includeFilters: [".*\\.js$"]
-        }
-      }
-    },
-
-    // Generate JavaScript documentation
-    jsdoc: {
-      'helix-core': {
-        src: [
-          '<%= sourceDir %>/js/helix-core/**/*.js',
-          '!<%= sourceDir %>/js/helix-core/__tests__/**/*.js',
-        ],
-        options: {
-          destination: 'jsdoc',
-        }
-      },
-    },
-
   });
 
   //
@@ -314,8 +265,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-apidoc');
-  grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-browserify');
 
   //
@@ -335,9 +284,6 @@ module.exports = function (grunt) {
     grunt.log.writeln('Building in [' + (process.env.NODE_ENV || 'development') + '] mode');
   });
 
-  // Build documentation
-  grunt.registerTask('docs', ['apidoc', 'jsdoc', 'copy:apidoc', 'copy:jsdoc']);
-
   // Transpile, bundle and minify JavaScript files
   grunt.registerTask('build:vendor', develop ?
     ['browserify:vendor-util', 'browserify:vendor-react'] :
@@ -350,8 +296,8 @@ module.exports = function (grunt) {
   grunt.registerTask('build', ['build:vendor', 'build:helix-core']);
 
   // Development build
-  grunt.registerTask('develop', ['mode', 'clean', 'build', 'docs', 'copy', 'watch']);
+  grunt.registerTask('develop', ['mode', 'clean', 'build', 'copy', 'watch']);
 
   // Production build
-  grunt.registerTask('default', ['mode', 'clean', 'build', 'docs', 'copy']);
+  grunt.registerTask('default', ['mode', 'clean', 'build', 'copy']);
 };
