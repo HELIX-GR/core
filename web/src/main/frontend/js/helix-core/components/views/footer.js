941b3583f13ada6d5e38ec9197084ab001f8bc9f
molly <katsouli.mo@gmail.com>
2024-07-13 12:19:51 +0300

    Adds logo for innovation

--- web/src/main/frontend/js/helix-core/components/views/footer.js
+++ web/src/main/frontend/js/helix-core/components/views/footer.js
@@ -70,6 +70,9 @@ class Footer extends React.Component {
             <div className="footer__logos">
               <a target="_blank" href="http://www.gsrt.gr/"><img src="/images/logos/footer_logo_1.png" alt="" /></a>
               <a target="_blank" href="http://www.mindev.gov.gr/"><img src="/images/logos/footer_logo_2.png" alt="" /></a>
+              <a target="_blank" href="#"><img src="/images/logos/footer_logo_3.png" alt="" /></a>
+
+              <p><FormattedMessage id="text.footer.national-innovation-program" /></p>
             </div>
             <div className="footer__social">
               <ul>
